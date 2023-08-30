73447d3f7f8f608de80ff4dac208cffacf6b8278
Emencosam <emenco04@gmail.com>
2023-08-30 11:40:13 -0500

    second commit

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
-resource "aws_ec2" "instance-1" {
-  
+resource "aws_instance" "web" {
+  ami           = "ami-0f409bae3775dc8e5"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "hello-world"
+  }
 }
+
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.14.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+  profile = "staging"
+}
+ 
+
+
+
+
+
