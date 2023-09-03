fd9fa4ec629b471fed6335c4a940e1eb4215f122
Emencosam <emenco04@gmail.com>
2023-09-02 21:14:20 -0500

    modified credential variables for terraform cloud

--- main.tf
+++ main.tf
@@ -26,9 +26,16 @@ terraform {
 
 
 provider "aws" {
-  region     = "us-east-1"
-  profile = "staging"
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+
+variable "AWS_REGION" {}
+
+variable "AWS_ACCESS_KEY_ID" {}
+
+variable "AWS_SECRET_ACCESS_KEY" {}
  
 
 
